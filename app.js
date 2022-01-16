1dbaca52fc81fec2223b9024332ddaf7c345c009
dasurahul <dasurahul598@gmail.com>
2022-01-16 12:40:34 +0530

    Added no scrolll functionality

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const close = document.querySelector(".close");
 
 menu.addEventListener("click", () => {
   menuContent.classList.add("block");
+  document.body.classList.add("full-height");
 });
 
 close.addEventListener("click", () => {
   menuContent.classList.remove("block");
+  document.body.classList.remove("full-height");
 });
